e6511a0270af39849d40b989671d2926091fe8b8
Don Cross <cosinekitty@gmail.com>
2021-08-24 11:15:27 -0400

    Starting to add an automated unit test suite.

--- src/Dns/README.md
+++ src/Dns/README.md
@@ -5,3 +5,10 @@ from his [DNS.NET Resolver project](https://www.codeproject.com/Articles/23673/D
 which was in turn modified for Claire Novotny's [Zeroconf](https://github.com/novotnyllc/Zeroconf)
 Bonjour/MDNS discovery project.
 
+# Modifications
+
+I, [Don Cross](https://github.com/cosinekitty/), have added some things useful for my own needs:
+
+- The `RecordWriter` class needed for serializing back to wire format for publishing.
+
+- Implemented support for [`NSEC` records](https://datatracker.ietf.org/doc/html/rfc4034#section-4.1).
---
+++ src/UnitTests/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace CosineKitty.ZeroConfigWatcher.UnitTests
+{
+    class Program
+    {
+        static bool Verbose;
+
+        static void Debug(string text)
+        {
+            if (Verbose)
+                Console.WriteLine(text);
+        }
+
+        static int Main(string[] args)
+        {
+            // Force use of "." for the decimal mark, regardless of local culture settings.
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            if (args.Length > 0 && args[0] == "-v")
+            {
+                Verbose = true;
+                args = args.Skip(1).ToArray();
+            }
+
+            if (args.Length == 1)
+            {
+                string name = args[0];
+                if (name == "all")
+                {
+                    Console.WriteLine("zctest: BEGIN");
+                    foreach (Test t in UnitTests)
+                        if (0 != t.Run())
+                            return 1;
+                    Console.WriteLine("zctest: SUCCESS");
+                    return 0;
+                }
+
+                foreach (Test t in UnitTests)
+                    if (t.Name == name)
+                        return t.Run();
+            }
+
+            Console.WriteLine("zctest: Invalid command line parameters.");
+            return 1;
+        }
+
+        struct Test
+        {
+            public readonly string Name;
+            private readonly Func<int> TestFunc;
+
+            public Test(string name, Func<int> testFunc)
+            {
+                this.Name = name;
+                this.TestFunc = testFunc;
+            }
+
+            public int Run()
+            {
+                int rc = TestFunc();
+                string result = (rc == 0) ? "PASS" : "FAIL";
+                Console.WriteLine($"zctest: {result} {Name}");
+                return rc;
+            }
+        }
+
+        static Test[] UnitTests = new Test[]
+        {
+            new Test("DoNothing", DoNothing),
+        };
+
+        static int DoNothing()
+        {
+            return 0;
+        }
+    }
+}
